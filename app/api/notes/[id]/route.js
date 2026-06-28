3b29326214d090f830ade16c8d55917228ecda93
NS <Nigus.Solomon.Dev@outlook.com>
2026-06-28 16:48:58 +0300

    Refactor routes to use controller functions instead of inline logic

--- app/api/notes/[id]/route.js
+++ app/api/notes/[id]/route.js
@@ -1,17 +1,8 @@
-import { NextResponse } from "next/server";
-import connectDB from "../../../../lib/mongoose";
-import Note from "../../../../models/Note";
+import { deletedNote,updatedNote } from "../../../../controllers/noteController.js";
 
 export async function DELETE(request, { params }) {
-  await connectDB();
-  try {
-    const { id } = await params;
-    const deleteNote = await Note.findByIdAndDelete(id);
-    if (!deleteNote) {
-      return NextResponse.json({ error: 'note not found' }, { status: 404 });
-    }
-    return NextResponse.json({ message: 'Note deleted successfully' });
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+  return deletedNote(request, { params });
+}
+export async function PUT(request, { params }) {
+  return updatedNote(request, { params });
 }
--- app/api/notes/route.js
+++ app/api/notes/route.js
@@ -1,35 +1,10 @@
-import { NextResponse } from "next/server";
-import connectDB from "../../../lib/mongoose.js";
-import Note from "../../../models/Note.js";
+import { getNotes,createNote } from "../../../controllers/noteController";
+
+
 
 export async function GET() {
-  await connectDB();
-  try{
-    const notes=await Note.find().sort({createdAt:-1});
-    return NextResponse.json(notes);
-  }catch(error){
-    return NextResponse.json({error:error.message},{status:500})
-  }
-  
+return getNotes();
 }
 export async function POST(request) {
-  await connectDB();
-  try{
-    const {title,content}=await request.json();
-    const newNote=new Note({
-      title:title,
-      content: content,
-    });
-    await newNote.save();
-    return NextResponse.json(
-      {message:'note created',note:newNote},
-      {status:201}
-    )
-  }catch(error){
-    return NextResponse.json(
-      {error:error.message},
-      {status:400}
-    )
-  }
-  
+ return createNote(request); 
 }
